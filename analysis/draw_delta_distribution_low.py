7a7d4b81bd1a673a69588e6456868b886c8d9c71
borissedlak <bsedlak01@gmail.com>
2024-02-19 14:03:04 +0100

    Also add boxplot for workers executed at server

--- analysis/draw_delta_distribution_low.py
+++ analysis/draw_delta_distribution_low.py
@@ -18,8 +18,7 @@ lines = []
 boxplot_position = 0
 constrained_thresh_O_X = 40 - (utils.get_latency_for_devices("Nano", "Orin") + utils.get_latency_for_devices("Orin", "Xavier"))
 # constrained_thresh_O_L = 40 - (utils.get_latency_for_devices("Nano", "Orin") + utils.get_latency_for_devices("Orin", "Laptop"))
-for index, col, thresh in [(0, 'dimgray', ("dashed", 45.75)), (2, 'firebrick', ("solid", constrained_thresh_O_X)),
-                           (3, 'chocolate', ("solid", constrained_thresh_O_X)), (5, 'steelblue', ("solid", 31))]:  # , (1, 'steelblue')
+for index, col, thresh in [(1, 'mediumaquamarine', ("dashed", 16.225)), (6, 'green', ("dashed", 16.225))]:  # , (1, 'steelblue')
     df_t = df_all[(df_all['t'] == index) & (df_all['service_name'] == "Processor") & (df_all['select'] == True)]
 
     pixel = df_t['pixel'].unique()[0]
@@ -30,7 +29,7 @@ for index, col, thresh in [(0, 'dimgray', ("dashed", 45.75)), (2, 'firebrick', (
     df_exp = pd.read_csv(f"../analysis/performance/{processor_host}.csv")
     df_exp = df_exp[(df_exp['pixel'] == pixel) & (df_exp['fps'] == fps)]
 
-    boxplot = ax.boxplot(list(df_exp['delta']), positions=[boxplot_position], patch_artist=True, widths=0.5, whis=1.5)
+    boxplot = ax.boxplot(list(df_exp['delta']), positions=[boxplot_position], patch_artist=True, widths=0.36)#, whis=[15, 85])
     boxplot['boxes'][0].set_facecolor(col)
     boxplots.append(boxplot)
 
@@ -43,12 +42,10 @@ for index, col, thresh in [(0, 'dimgray', ("dashed", 45.75)), (2, 'firebrick', (
 
 ax.set_ylabel(r'$\mathit{Worker}$ processing delay (ms)')
 ax.legend([box['boxes'][0] for box in boxplots] + lines,
-          [r'Worker @ $\mathit{Xavier}$', r'Worker @ $\mathit{Orin}$', r'Worker @ $\mathit{Orin}$', r'Worker @ $\mathit{Xavier}$',
-           'Virtual threshold', 'Delay threshold'])
-ax.set_ylim(30.5, 46)
-fig.set_size_inches(4.5, 4.8)
-plt.xticks([0, 1, 2, 3], [r'$\mathit{t_0}$', r'$\mathit{t_2}$', r'$\mathit{t_3}$',
-                          r'$\mathit{t_5}$'])
+          [r'Worker @ $\mathit{Server}$', r'Worker @ $\mathit{Server}$', 'Virtual threshold'])
+ax.set_ylim(14.9, 16.25)
+fig.set_size_inches(3.2, 4.8)
+plt.xticks([0, 1], [r'$\mathit{t_1}$', r'$\mathit{t_6}$'])
 # Show the plot
-plt.savefig("./plots/delta_distribution_high.eps", dpi=600, bbox_inches="tight", format="eps")  # default dpi is 100
+plt.savefig("./plots/delta_distribution_low.eps", dpi=600, bbox_inches="tight", format="eps")  # default dpi is 100
 plt.show()
