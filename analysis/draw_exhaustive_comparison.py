b605771d59fef047025b184ee82ff3dc845680c0
boris PC <b.sedlak@dsg.tuwien.ac.at>
2024-02-20 19:50:30 +0100

    change colors and labels of the exhaustive comparison figure

--- analysis/draw_exhaustive_comparison.py
+++ analysis/draw_exhaustive_comparison.py
@@ -15,16 +15,16 @@ fig, ax = plt.subplots()
 df = pd.read_csv("./performance/comparison_slo.csv")
 df_all = pd.read_csv('../analysis/inference/n_n_assignments.csv')
 
+labelled = []
+first_entry = True
 for index, label, col in [(2, r'$\mathit{t_2}$', 'firebrick'), (3, r'$\mathit{t_3}$', 'chocolate'),
                           (4, r'$\mathit{t_4}$', 'mediumaquamarine')]:
-    first_entry = True
     df_t = df[(df['t'] == index) & ((df['service'] == "Processor") | (df['service'] == "Consumer_C"))
               & (df['modus'] == "experienced")]
     experienced_slo = df_t['slo'].sum()
 
-    plt.hlines(experienced_slo, xmin=index - 0.4, xmax=index + 0.4, color=col, linestyle='-',
-               label=label + " selected",
-               linewidth=3)  # , alpha=0.5)
+    line = plt.hlines(experienced_slo, xmin=index - 0.4, xmax=index + 0.4, color='steelblue', linestyle='-',
+                      linewidth=3)  # , alpha=0.5)
 
     part = df_all[(df_all['t'] == index)]
     device_list = list(part['host'].unique())
@@ -34,9 +34,9 @@ for index, label, col in [(2, r'$\mathit{t_2}$', 'firebrick'), (3, r'$\mathit{t_
     min_latency = part['min_latency'].unique()[0]  # Increase resolution a bit here
     fps = part['fps'].unique()[0]
 
-    lines = []
+    experiments = []
     for (processor, consumer) in itertools.product(device_list, device_list):
-        if consumer in ['PC', 'Laptop'] :
+        if consumer in ['PC', 'Laptop']:
             continue
 
         df_exh = pd.read_csv(f"../analysis/performance/{processor}.csv")
@@ -54,19 +54,26 @@ for index, label, col in [(2, r'$\mathit{t_2}$', 'firebrick'), (3, r'$\mathit{t_
             rate += random.uniform(0.0, 0.15)  # Improve resolution
         print(rate, processor, consumer)
 
-        lines.append(rate)
+        experiments.append(rate)
         # plt.hlines(rate, xmin=index - 0.5, xmax=index + 0.5, color=col, linestyle='--',
         #            linewidth=0.8, label="alternatives" if first_entry else None)
-        # first_entry = False
-    boxplot = ax.boxplot(lines, positions=[index], patch_artist=True, widths=0.36, whis=1.35)
-    # boxplot = ax.boxplot(list(df_exp['delta']), positions=[boxplot_position], patch_artist=True, widths=0.36, whis=1.35)
-    boxplot['boxes'][0].set_facecolor(col)
+    boxplot = plt.boxplot(experiments, positions=[index], patch_artist=True, widths=0.36, whis=1.35)
+    boxplot['boxes'][0].set_facecolor('firebrick')
+
+    if first_entry:
+        labelled.append(line)
+        labelled.append(boxplot)
+        first_entry = False
+
     print("------------\n")
 
-plt.ylabel(r'% SLO fulfillment; $\mathit{W}$ + $\mathit{C_3}$')
-plt.legend(loc='upper right')
+ax.legend([labelled[0]] + [labelled[1]['boxes'][0]],
+          ['Selected assignment', 'Alternative assignments', 'Virtual threshold', 'Delay threshold'])
+
+plt.ylabel(r'SLO fulfillment; $\mathit{W}$ + $\mathit{C_3}$')
+# plt.legend(loc='upper right')
 ax.set_ylim(-0.02, 1.58)
-fig.set_size_inches(5.8, 2.7)
+fig.set_size_inches(5.8, 2.5)
 plt.xticks([2, 3, 4], [r'$\mathit{t_2}$', r'$\mathit{t_3}$', r'$\mathit{t_4}$'])
 
 plt.savefig("./plots/exhaustive_comparison_slo.eps", dpi=600, bbox_inches="tight", format="eps")  # default dpi is 100
