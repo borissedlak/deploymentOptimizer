2682ce60a300a85afd7c6a8664e5f1c00e822657
borissedlak <bsedlak01@gmail.com>
2024-02-20 16:23:56 +0100

    change to boxplots from hlines

--- analysis/draw_exhaustive_comparison.py
+++ analysis/draw_exhaustive_comparison.py
@@ -22,7 +22,7 @@ for index, label, col in [(2, r'$\mathit{t_2}$', 'firebrick'), (3, r'$\mathit{t_
               & (df['modus'] == "experienced")]
     experienced_slo = df_t['slo'].sum()
 
-    plt.hlines(experienced_slo, xmin=index - 0.5, xmax=index + 0.5, color=col, linestyle='-',
+    plt.hlines(experienced_slo, xmin=index - 0.4, xmax=index + 0.4, color=col, linestyle='-',
                label=label + " selected",
                linewidth=3)  # , alpha=0.5)
 
@@ -34,8 +34,9 @@ for index, label, col in [(2, r'$\mathit{t_2}$', 'firebrick'), (3, r'$\mathit{t_
     min_latency = part['min_latency'].unique()[0]  # Increase resolution a bit here
     fps = part['fps'].unique()[0]
 
+    lines = []
     for (processor, consumer) in itertools.product(device_list, device_list):
-        if consumer in ['PC', 'Laptop'] or (consumer == "Orin" and processor == "Orin"):
+        if consumer in ['PC', 'Laptop'] :
             continue
 
         df_exh = pd.read_csv(f"../analysis/performance/{processor}.csv")
@@ -53,14 +54,18 @@ for index, label, col in [(2, r'$\mathit{t_2}$', 'firebrick'), (3, r'$\mathit{t_
             rate += random.uniform(0.0, 0.15)  # Improve resolution
         print(rate, processor, consumer)
 
-        plt.hlines(rate, xmin=index - 0.5, xmax=index + 0.5, color=col, linestyle='--',
-                   linewidth=0.8, label="alternatives" if first_entry else None)
-        first_entry = False
+        lines.append(rate)
+        # plt.hlines(rate, xmin=index - 0.5, xmax=index + 0.5, color=col, linestyle='--',
+        #            linewidth=0.8, label="alternatives" if first_entry else None)
+        # first_entry = False
+    boxplot = ax.boxplot(lines, positions=[index], patch_artist=True, widths=0.36, whis=1.35)
+    # boxplot = ax.boxplot(list(df_exp['delta']), positions=[boxplot_position], patch_artist=True, widths=0.36, whis=1.35)
+    boxplot['boxes'][0].set_facecolor(col)
     print("------------\n")
 
 plt.ylabel(r'% SLO fulfillment; $\mathit{W}$ + $\mathit{C_3}$')
 plt.legend(loc='upper right')
-ax.set_ylim(-0.02, 1.6)
+ax.set_ylim(-0.02, 1.58)
 fig.set_size_inches(5.8, 2.7)
 plt.xticks([2, 3, 4], [r'$\mathit{t_2}$', r'$\mathit{t_3}$', r'$\mathit{t_4}$'])
 
