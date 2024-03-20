c908bc446bc49607f1303903f26723e0d6b1e044
borissedlak <bsedlak01@gmail.com>
2024-03-20 17:28:49 +0100

    experiment a bit

--- SOSE/C_Traffic_Precition/inference.py
+++ SOSE/C_Traffic_Precition/inference.py
@@ -1,6 +1,8 @@
 from pgmpy.inference import VariableElimination
 from pgmpy.readwrite import XMLBIFReader
 
+from detector import utils
+
 # model = XMLBIFReader("./model_anomaly.xml").get_model()
 # ve = VariableElimination(model)
 # result = ve.query(variables=['cumm_net_delay_True'], evidence={'delta': '61'})  # 61 + 9 = 70...
@@ -14,6 +16,7 @@ from pgmpy.readwrite import XMLBIFReader
 
 
 model = XMLBIFReader("./model_analysis.xml").get_model()
+utils.export_BN_to_graph(model)
 ve = VariableElimination(model)
 # result = ve.query(variables=['cumm_net_delay_True'], evidence={'delta': '61'})  # 61 + 9 = 70...
 # print(result)
@@ -23,7 +26,14 @@ ve = VariableElimination(model)
 result = ve.query(variables=['pixel'], evidence={'cumm_net_delay_True': 'True'})
 print(result)
 
-result = ve.query(variables=['cpu'], evidence={'pixel': '1080', 'fps': '35'})
+# result = ve.query(variables=['cpu'], evidence={'pixel': '1080', 'fps': '35'})
+# print(result)
+# result = ve.query(variables=['cpu'], evidence={'pixel': '480', 'fps': '15'})
+# print(result)
+result = ve.query(variables=['cpu'], evidence={'consumption': '6'})
 print(result)
-result = ve.query(variables=['cpu'], evidence={'pixel': '480', 'fps': '15'})
+result = ve.query(variables=['cpu'], evidence={'consumption': '7'})
 print(result)
+
+# result = ve.query(variables=['cpu'], evidence={'pixel': '1080', 'fps': '35'})
+# print(result)
--- SOSE/C_Traffic_Precition/train_BN.py
+++ SOSE/C_Traffic_Precition/train_BN.py
@@ -10,8 +10,8 @@ df_analysis = pd.read_csv('../PW_Street_Analysis/W_metrics_analysis.csv')
 df_analysis = shuffle(pd.concat([df_analysis, df_analysis]), random_state=35)  # To get almost 10.000 samples
 
 # del df_analysis['cpu']
-del df_analysis['gpu']
-del df_analysis['memory']
+# del df_analysis['gpu']
+# del df_analysis['memory']
 del df_analysis['in_time']
 
 
@@ -28,9 +28,9 @@ df_analysis['cumm_net_delay_True'] = df_analysis['cumm_net_delay'] <= 50
 
 utils.train_to_BN(df_analysis, "Traffic Prediction", export_file="./model_analysis.xml")
 
-del df_anomaly['cpu']
-del df_anomaly['gpu']
-del df_anomaly['memory']
+# del df_anomaly['cpu']
+# del df_anomaly['gpu']
+# del df_anomaly['memory']
 del df_anomaly['timestamp']
 
 df_anomaly['cumm_net_delay'] = df_anomaly.apply(calculate_cumulative_net_delay, axis=1, args=("Xavier", "Laptop",))
