0a573e837c39c86ea4073b961709e523d44aa345
borissedlak <bsedlak01@gmail.com>
2024-03-21 17:21:05 +0100

    extract weather data from isentropic analysis and train BN

--- SOSE/C_Traffic_Precition/train_BN.py
+++ SOSE/C_Traffic_Precition/train_BN.py
@@ -7,12 +7,16 @@ from detector.utils import get_latency_for_devices
 df_anomaly = shuffle(pd.read_csv('../PW_Traffic/W_metrics_anomaly.csv'), random_state=35)
 
 df_analysis = pd.read_csv('../PW_Street_Analysis/W_metrics_analysis.csv')
-df_analysis = shuffle(pd.concat([df_analysis, df_analysis]), random_state=35)  # To get almost 10.000 samples
+df_analysis = shuffle(pd.concat([df_analysis] * 2), random_state=35)  # To get almost 10.000 samples
 
 # del df_analysis['cpu']
 # del df_analysis['gpu']
 # del df_analysis['memory']
 del df_analysis['in_time']
+
+
+# TODO: Do I put them all in one BN?
+# TODO: In any case, I should create a loop for the following BNs
 
 
 def calculate_cumulative_net_delay(row, src, dest):
@@ -26,7 +30,9 @@ df_analysis['cumm_net_delay_True'] = df_analysis['cumm_net_delay'] <= 50
 # df_analysis['delta_True'] = df_analysis['delta'] <= 61
 # print(df_analysis)
 
-utils.train_to_BN(df_analysis, "Traffic Prediction", export_file="./model_analysis.xml")
+# utils.train_to_BN(df_analysis, "Traffic Prediction", export_file="./model_analysis.xml")
+
+#########################################################
 
 # del df_anomaly['cpu']
 # del df_anomaly['gpu']
@@ -39,4 +45,22 @@ print(df_anomaly)
 
 # df = pd.merge(df_anomaly, df_analysis, left_index=True, right_index=True)  # Only joins to the max of one list
 
-utils.train_to_BN(df_anomaly, "Traffic Prediction", export_file="./model_anomaly.xml")
+# utils.train_to_BN(df_anomaly, "Traffic Prediction", export_file="./model_anomaly.xml")
+
+#########################################################
+
+df_weather = pd.read_csv('../PW_Weather_Transport/W_metrics_weather.csv')
+df_weather = shuffle(pd.concat([df_weather] * 6), random_state=35)  # To get almost 10.000 samples
+
+# del df_anomaly['cpu']
+# del df_anomaly['gpu']
+# del df_anomaly['memory']
+del df_weather['timestamp']
+
+# df_weather['cumm_net_delay'] = df_weather.apply(calculate_cumulative_net_delay, axis=1, args=("Xavier", "Laptop",))
+# df_weather['cumm_net_delay_True'] = df_weather['cumm_net_delay'] <= 50
+print(df_weather)
+
+# df = pd.merge(df_anomaly, df_analysis, left_index=True, right_index=True)  # Only joins to the max of one list
+
+utils.train_to_BN(df_weather, "Traffic Prediction", export_file="./model_weather.xml")
