178f1f116c779d0a533cd31dfaf74bb033d53dae
boris PC <b.sedlak@dsg.tuwien.ac.at>
2024-03-22 08:35:58 +0100

    add last service (i.e., cloud retrieval) :-)

--- SOSE/C_Traffic_Precition/train_BN.py
+++ SOSE/C_Traffic_Precition/train_BN.py
@@ -41,7 +41,6 @@ del df_anomaly['timestamp']
 
 df_anomaly['cumm_net_delay'] = df_anomaly.apply(calculate_cumulative_net_delay, axis=1, args=("Xavier", "Laptop",))
 df_anomaly['cumm_net_delay_True'] = df_anomaly['cumm_net_delay'] <= 50
-print(df_anomaly)
 
 # df = pd.merge(df_anomaly, df_analysis, left_index=True, right_index=True)  # Only joins to the max of one list
 
@@ -49,7 +48,7 @@ print(df_anomaly)
 
 #########################################################
 
-df_weather = pd.read_csv('../PW_Weather_Transport/W_metrics_weather.csv')
+df_weather = pd.read_csv('../PW_Weather/W_metrics_weather.csv')
 # Idea: if I actually keep them separate, then the latter is obsolete, no?
 df_weather = shuffle(pd.concat([df_weather] * 6), random_state=35)  # To get almost 10.000 samples
 
@@ -60,11 +59,28 @@ del df_weather['timestamp']
 
 df_weather['cumm_net_delay'] = df_weather.apply(calculate_cumulative_net_delay, axis=1, args=("Xavier", "Laptop",))
 # df_weather['cumm_net_delay_True'] = df_weather['cumm_net_delay'] <= 50
-print(df_weather)
 
 # df = pd.merge(df_anomaly, df_analysis, left_index=True, right_index=True)  # Only joins to the max of one list
 
-utils.train_to_BN(df_weather, "Traffic Prediction", export_file="./model_weather.xml")
+# utils.train_to_BN(df_weather, "Traffic Prediction", export_file="./model_weather.xml")
+
+#########################################################
+
+df_cloud = pd.read_csv('../PW_Cloud_DB/W_metrics_cloud.csv')
+df_cloud = shuffle(pd.concat([df_cloud] * 5), random_state=35)  # To get almost 10.000 samples
+
+# del df_anomaly['cpu']
+# del df_anomaly['gpu']
+# del df_anomaly['memory']
+del df_cloud['timestamp']
+
+df_cloud['cumm_net_delay'] = df_cloud.apply(calculate_cumulative_net_delay, axis=1, args=("PC", "Laptop",))
+# df_weather['cumm_net_delay_True'] = df_weather['cumm_net_delay'] <= 50
+print(df_cloud)
+
+# df = pd.merge(df_anomaly, df_analysis, left_index=True, right_index=True)  # Only joins to the max of one list
+
+utils.train_to_BN(df_cloud, "Traffic Prediction", export_file="./model_cloud.xml")
 
 #########################################################
 
